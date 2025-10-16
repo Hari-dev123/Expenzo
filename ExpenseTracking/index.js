e0f185dec1acbe29c83a331b994c27198e80012e
= <=>
2025-10-16 22:28:03 +0530

    changes made

--- ExpenseTracking/index.js
+++ ExpenseTracking/index.js
@@ -1,33 +1,28 @@
-const express=require('express')
+const express = require("express");
 const cors = require("cors");
-const User=require('./Models/userModel.js')
-const UserRoute=require('./Routes/route.js')
-const connectDb=require('./config/db.js')
-require('dotenv').config()
-
+const User = require("./Models/userModel.js");
+const UserRoute = require("./Routes/route.js");
+const connectDb = require("./config/db.js");
+require("dotenv").config();
 
 connectDb();
-const app =express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,  
+    origin: process.env.FRONTEND_URL,
     methods: "GET,POST,PUT,DELETE,PATCH",
-    credentials: true
+    credentials: true,
   })
 );
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/api",UserRoute);
-app.get('/',(req,res)=>{
-    res.send("hellon")
-})
-
-
-
-
-app.listen(3000,()=>{
-    console.log("server is running")
-})
-
+app.use("/api", UserRoute);
+app.get("/", (req, res) => {
+  res.send("hellon");
+});
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log("server is running");
+});
